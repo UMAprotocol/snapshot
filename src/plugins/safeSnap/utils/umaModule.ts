39e7345fdf1d19ae4e9a6c6e641e583faa4cf9ee
John Shutt <pemulis@users.noreply.github.com>
2023-01-12 12:07:05 -0800

    fix: return first value from single-element array
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- src/plugins/safeSnap/utils/umaModule.ts
+++ src/plugins/safeSnap/utils/umaModule.ts
@@ -244,7 +244,7 @@ export const getModuleDetailsUma = async (
     needsBondApproval: needsApproval,
     noTransactions: false,
     activeProposal: activeProposal,
-    proposalEvent: thisModuleFullProposalEvent,
+    proposalEvent: thisModuleFullProposalEvent[0],
     proposalExecuted: proposalExecuted
   };
 };
